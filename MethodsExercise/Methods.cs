689c0529b649a799f9b768b8bfd666b53757b505
yvonnehuynh79 <yvonnehuynh888@gmail.com>
2023-04-10 11:53:00 -0700

    first commit

---
+++ MethodsExercise/Methods.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace MethodsExercise
+{
+	public class Methods
+	{
+		public static void Add(int num1,int num2)
+		{
+			int sum = num1 + num2;
+			Console.WriteLine(sum);
+
+		}
+
+		public static void Subtract(int num1, int num2)
+		{
+			int sum = num1 - num2;
+			Console.WriteLine(sum);
+
+		}
+			
+		public static void Multiply(int num1, int num2)
+		{
+			int sum = num1 * num2;
+			Console.WriteLine(sum);
+
+		}
+		public static void Divide(int num1, int num2)
+		{
+			int sum = num1 / num2;
+			Console.WriteLine(sum);
+
+		}
+	}
+}
+
--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Methods.Add(5, 5);
+            Methods.Subtract(10, 2);
+            Methods.Multiply(2, 7);
+            Methods.Divide(7, 2);
+
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite color?");
+            string color = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite animal?");
+            string animal = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite TV show?");
+            string TvShow = Console.ReadLine();
+
+            Console.WriteLine($"Hi, my name is {name}, I like all the colors,\n +" +
+                $" but if I have to pick one as a favorite color it would be {color}.\n+" +
+                $"My favorite animal is {animal}.  And my favorite TV show of all times is{TvShow}.");
+
+
+
+
+       
+
         }
     }
 }
